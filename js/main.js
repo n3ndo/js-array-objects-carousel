8154b260fae4b0fc14c0ecc2f5c3ef13017c145b
n3ndo <ferdinandof3lixbagnale@gmail.com>
2023-12-06 08:37:16 +0100

    [UPDATE] Milestone1 and Milestone2 done

--- js/main.js
+++ js/main.js
@@ -21,3 +21,39 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+let indice = 0;
+
+function changeImage() {
+    const imgElement = document.createElement('img');
+    imgElement.src = images[indice].image;
+    imgElement.alt = images[indice].title;
+
+    const imgContainer = document.getElementById('item-active');
+    imgContainer.innerHTML = '';
+
+    const titleElement = document.createElement('h2');
+    titleElement.textContent = images[indice].title;
+
+    const textElement = document.createElement('p');
+    textElement.textContent = images[indice].text;
+
+    imgContainer.appendChild(imgElement);
+    imgContainer.appendChild(titleElement);
+    imgContainer.appendChild(textElement);
+}
+
+function nextImage() {
+    indice = (indice + 1) % images.length;
+    changeImage();
+}
+
+function prevImage() {
+    indice = (indice - 1 + images.length) % images.length;
+    changeImage();
+}
+
+const next = document.getElementById('next').addEventListener('click', nextImage);
+const prev = document.getElementById('prev').addEventListener('click', prevImage);
+
+changeImage();
